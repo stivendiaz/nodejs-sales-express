4f2587bd99bcd0da3df3f7022bec8f0cee2c1491
stivendiaz <stivendiazagudelo@gmail.com>
2022-09-29 20:05:53 -0500

    Add server configuration

---
+++ src/router.ts
@@ -0,0 +1,8 @@
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+router
+  .get('/', (req: Request, res: Response) => res.send('Hello World!'))
+
+export default router;
